15d21c5216bd1dd9799c32dbd31239114d85cc25
Daniel Stern <daniel@danielstern.ca>
2016-12-17 11:22:54 -0500

    Implemented store

--- src/control-panel.js
+++ src/control-panel.js
@@ -1,1 +1,41 @@
-console.log(`control-panel.js`);
+import { Dispatcher, Store } from './flux';
+
+const controlPanelDispatcher = new Dispatcher();
+
+export const UPDATE_USERNAME = `UPDATE_USERNAME`;
+export const UPDATE_FONT_SIZE_PREFERENCE = `UPDATE_FONT_SIZE_PREFERENCE`;
+
+document.forms.fontSizeForm.fontSize.forEach(element=>{
+    element.addEventListener("change",({target})=>{
+        controlPanelDispatcher.dispatch(UPDATE_USERNAME);
+    })
+});
+
+document.getElementById(`userNameInput`).addEventListener("input",({target})=>{
+    const name = target.value;
+    controlPanelDispatcher.dispatch(UPDATE_FONT_SIZE_PREFERENCE);
+});
+
+class UserPrefsStore extends Store {
+    getInitialState() {
+        return {
+            userName: "Jim",
+            fontSize: "small"
+        };
+    }
+    __onDispatch(action){
+        console.info(`Received dispatch`, action);
+        this.__emitChange();
+    }
+    getUserPreferences(){
+        return this.__state;
+    }
+}
+
+const userPrefsStore = new UserPrefsStore(controlPanelDispatcher);
+
+userPrefsStore.addListener((state)=>{
+    console.log(`Updated Store`,state);
+});
+
+
---
+++ src/flux/index.js
@@ -0,0 +1,2 @@
+export {Dispatcher} from './Dispatcher';
+export {Store} from './Store';
